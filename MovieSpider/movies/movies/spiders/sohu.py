80dd0cdebc0e7a200fbdebb9894685b2addb6af3
xuaokun <xuaokun1997@163.com>
2021-10-05 14:59:13 +0800

    fix:搜狐新闻author字段部分为空

--- MovieSpider/movies/movies/spiders/sohu.py
+++ MovieSpider/movies/movies/spiders/sohu.py
@@ -11,11 +11,6 @@ class ExampleSpider(scrapy.Spider):
     allowed_domains = ['sohu.com'] #过滤不在范围内的域名
     start_urls = ['http://news.sohu.com/']
     def parse(self, response):
-        # content = response.body
-        # f = open("qqmusic_toplist.html","wb+")
-        # f.write(content)
-        # f.close()
-        return
         nodes = response.xpath('//a/@href')
         itemsCnt = 0
         for node in nodes:
@@ -23,28 +18,19 @@ class ExampleSpider(scrapy.Spider):
             if url and ('sohu.com' in str(url)):
                 itemsCnt += 1
                 print('本次爬取第{}条数据'.format(++itemsCnt))
-                # if(itemsCnt > 10):
-                #     break
                 yield Request(url=url, callback=self.parse_detail)
 
     def parse_detail(self, response):
         f = open("sohu.txt", "a+")
         item = NewsItem()
         url = response.url
-        # print(url)
-        # print('新闻详情页：')
         node = response.xpath('//div[@data-spm="content"]')
         if node and node[0]:
             title = node.css('.text-title > h1::text').extract_first().strip() \
                     or node.css('span.title-info-title::text').extract_first().strip()
-            # print(title)
-            # print('-----')
             date = node.css('.time::text').extract_first()
-            # print(date)
-            # print('-----')
-            author = node.css('span[data-role="original-link"] > a::text').extract_first()
-            # print(author)
-            # print('-----')
+            author = node.css('span[data-role="original-link"] > a::text,'
+                              'span[data-role="original-link"]::text').extract_first()
             contentList = node.css('article strong::text,article p::text').extract()
             content = ''
             for par in contentList:
@@ -55,12 +41,10 @@ class ExampleSpider(scrapy.Spider):
                 if not par.strip():
                     continue
                 content += par.strip() + '\n'
-            # print(content)
             contentHot = node.css('#mpbox > div.c-comment-content > div > div:nth-child(2) > div').extract()
-            print(contentHot)
             item['url'] = url
             item['title'] = title
-            item['date'] = date
+            item['date'] = date + ':00'
             item['author'] = author
             item['content'] = content
             yield item
